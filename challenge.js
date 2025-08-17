e84cf5c94ce89d2ee2a294294ace0e3f890dc669
dahunsi halima <dahunsiolajumoke18@gmail.com>
2025-08-17 10:11:44 +0100

    mobile responsiveness updated

--- challenge.js
+++ challenge.js
@@ -51,9 +51,19 @@ localStorage.setItem("products", JSON.stringify(product))
 
 const products = JSON.parse(localStorage.getItem("products"))
 
-const disitem = JSON.parse(localStorage.getItem("displayItem")) || products
+const disitem = JSON.parse(localStorage.getItem("displayItem")) || product[0]
 
-const displayItem = disitem[0].images[0]
+const displayItem = disitem.images[0]
+// if (localStorage.getItem("displayItem")) {
+//     const displayItem = disitem.images[0]
+
+// }
+// else {
+//     const disitem = product[0]
+
+//     const displayItem = disitem.images[0]
+
+// }
 // const next = () => {
 //     const displayingProduct = JSON.parse(localStorage.getItem("displayItem")) || products
 //     // const displayProduct = products.filter(item => item.id === id)
@@ -71,6 +81,7 @@ const displayItem = disitem[0].images[0]
 //     }
 // }
 const screenResize = () => {
+
     if (window.innerWidth >= 768) {
         const maain = document.getElementById("main");
         maain.innerHTML = `
@@ -90,16 +101,19 @@ const screenResize = () => {
         })
     }
     else {
+
+        let currentItemIndex = localStorage.getItem("itemIndex") || 0
         const maain = document.getElementById("main");
+
         maain.innerHTML = `
         <div class="svg">
-        <img id="svg-icon" src="assets/images/icon-previous.svg" alt="">
-    <img id="svg-icon" src="assets/images/icon-next.svg" alt="" onclick ="next()">
+        <img class="svg-icon" id="previous" src="assets/images/icon-previous.svg" alt="">
+    <img class="svg-icon " id="next"  src="assets/images/icon-next.svg" alt="" >
 
       </div>
     <div>
     
-    <img src="./${displayItem}" alt="">
+    <img src="./${product[currentItemIndex].images[0]}" alt="">
 
     </div>
     <div id="display">
@@ -107,7 +121,35 @@ const screenResize = () => {
     </div>
     
     `
+        const nextBtn = document.getElementById("next")
+        const prevBtn = document.getElementById("previous")
+        // const mobileDisplay = () => {
+        // for (let i = 0; i < nextBtn.length; i++) {
+
+        nextBtn.addEventListener("click", () => {
+
+            console.log(displayItem)
+
+            currentItemIndex++;
+            if (currentItemIndex < product.length) {
+                console.log(currentItemIndex)
+            }
+            else {
+                currentItemIndex = 0
+            }
+            // const index = localStorage.getItem("itemIndex")
+            // const ite = product.findIndex(item => item.id === index + 1)
+            // const prod = product.filter(item => item.id == ite + 2)
+            localStorage.setItem("itemIndex", currentItemIndex)
+            localStorage.setItem("displayItem", JSON.stringify(product[currentItemIndex]))
+            // console.log(ite)
+
+            window.location.href = "index.html"
+        })
     }
+
+
+
 }
 // 
 window.addEventListener("resize", screenResize);
@@ -115,7 +157,7 @@ window.addEventListener("load", screenResize);
 let existingItem = [];
 localStorage.setItem("itemInCartQty", JSON.stringify(existingItem))
 const imageLoad = (id) => {
-    const displayProduct = products.filter(item => item.id === id)
+    const displayProduct = products.find(item => item.id === id)
     // console.log(displayProduct[0].quantity)
     localStorage.setItem("displayItem", JSON.stringify(displayProduct))
     window.location.href = "index.html"
@@ -134,28 +176,28 @@ carrt.innerHTML = ` <div id="increament">
 
 <b onclick="decreament()">-</b>
 </div>
-<input id="cart" type="button" value="Add to cart" type="click" item="${disitem[0].id
-    }" onclick="addToCart(${disitem[0].id})">
+<input id="cart" type="button" value="Add to cart" type="click" item="${disitem.id
+    }" onclick="addToCart(${disitem.id})">
 <div >
 <!-- <img src="./assets/images/icon-cart.svg" alt=""> -->
 <!-- <p></p> -->
 </div>`
-console.log(disitem[0].id);
+console.log(disitem.id);
 //  existingItem = [];
 // localStorage.setItem("itemInCartQty", JSON.stringify(existingItem))
-let productL = products.find((item) => item.id === disitem[0].id);
+let productL = products.find((item) => item.id === disitem.id);
 
 function addToCart() {
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    let productL = products.find((item) => item.id === disitem[0].id);
+    let productL = products.find((item) => item.id === disitem.id);
     if (!productL) {
         console.log("product not found");
-        console.log(disitem[0].id);
+        console.log(disitem.id);
 
         return;
     }
 
-    existingItem = cart.find((item) => item.id === disitem[0].id);
+    existingItem = cart.find((item) => item.id === disitem.id);
 
     if (existingItem) {
         // existingItem.quantity = existingItem.quantity + 1;
@@ -177,7 +219,7 @@ function addToCart() {
     console.log(existingItem);
     localStorage.setItem("cart", JSON.stringify(cart));
     localStorage.setItem("itemInCartQty", JSON.stringify(existingItem))
-    let productLl = cart.find((item) => item.id === disitem[0].id);
+    let productLl = cart.find((item) => item.id === disitem.id);
 }
 const itemInCart = JSON.parse(localStorage.getItem("itemInCartQty")) || []
 
@@ -190,13 +232,13 @@ else {
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 // document.getElementById("qty").innerHTML = existingItem.quantity
-existingItem = cart.find((item) => item.id === disitem[0].id) || productL;
+existingItem = cart.find((item) => item.id === disitem.id) || productL;
 
 document.getElementById("cartIcon").innerHTML = cart.length
 document.getElementById("qty").innerHTML = existingItem.quantity || 0
 
 const increament = () => {
-    const existingItem = cart.find((item) => item.id === disitem[0].id);
+    const existingItem = cart.find((item) => item.id === disitem.id);
     if (existingItem) {
         existingItem.quantity += 1;
         localStorage.setItem("cart", JSON.stringify(cart));
@@ -210,18 +252,18 @@ const increament = () => {
     }
     const price = document.getElementById("price")
     price.innerHTML = ` <div>
-<h3>$${disitem[0].price * existingItem.quantity}</h3> 
+<h3>$${disitem.price * existingItem.quantity}</h3> 
 
 <p class="discount">50%</p>
 </div> 
-<p id="oldPrice">$${disitem[0].oldPrice} </p>
+<p id="oldPrice">$${disitem.oldPrice} </p>
 `
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 // decreament
 const decreament = () => {
-    const existingItem = cart.find((item) => item.id === disitem[0].id);
+    const existingItem = cart.find((item) => item.id === disitem.id);
     if (existingItem && existingItem.quantity > 1) {
         existingItem.quantity -= 1;
         localStorage.setItem("cart", JSON.stringify(cart));
@@ -235,11 +277,11 @@ const decreament = () => {
 
     const price = document.getElementById("price")
     price.innerHTML = ` <div>
-<h3>$${disitem[0].price * existingItem.quantity}</h3> 
+<h3>$${disitem.price * existingItem.quantity}</h3> 
 
 <p class="discount">50%</p>
 </div> 
-<p id="oldPrice">$${disitem[0].oldPrice} </p>
+<p id="oldPrice">$${disitem.oldPrice} </p>
 `
     localStorage.setItem("cart", JSON.stringify(cart));
 
@@ -251,18 +293,18 @@ const decreament = () => {
 const price = document.getElementById("price")
 let totalAmount
 if (existingItem && existingItem.quantity > 0) {
-    totalAmount = disitem[0].price * existingItem.quantity
+    totalAmount = disitem.price * existingItem.quantity
 }
-else { totalAmount = disitem[0].price }
+else { totalAmount = disitem.price }
 
 price.innerHTML = ` <div>
 <h3>$${totalAmount}</h3> 
 <p class="discount">50%</p>
 </div> 
-<p id="oldPrice">$${disitem[0].oldPrice} </p>
+<p id="oldPrice">$${disitem.oldPrice} </p>
 `
 let amount = cart.reduce((total, item) => total + item.price, 0);
-document.getElementById("totalPrice").innerHTML = amount
+// document.getElementById("totalPrice").innerHTML = amount
 console.log(amount);
 
 // display side nav
@@ -277,3 +319,7 @@ const displaynav = () => {
         aside.classList.add("hide")
     }
 }
+
+
+
+// }
